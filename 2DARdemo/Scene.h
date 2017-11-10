7428afb49ee618ba38ce7d2b18667cf88f15780e
hanfengs <yifan1223@gmail.com>
2017-11-10 15:41:47 +0800

    Initial Commit

---
+++ 2DARdemo/Scene.h
@@ -0,0 +1,14 @@
+//
+//  Scene.h
+//  2DARdemo
+//
+//  Created by hanfeng on 2017/11/10.
+//  Copyright © 2017年 hanfeng. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+#import <ARKit/ARKit.h>
+
+@interface Scene : SKScene
+
+@end
---
+++ 2DARdemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  2DARdemo
+//
+//  Created by hanfeng on 2017/11/10.
+//  Copyright © 2017年 hanfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+#import <ARKit/ARKit.h>
+
+@interface ViewController : UIViewController
+
+@end
